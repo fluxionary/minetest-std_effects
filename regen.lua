6ed6e4eb8212c370b286d7e5014b722c43056c73
flux <25628292+fluxionary@users.noreply.github.com>
2023-04-04 18:30:15 -0700

    regen tick at 1 second, no need to catch up

--- regen.lua
+++ regen.lua
@@ -1,17 +1,19 @@
+local s = std_effects.settings
 local S = std_effects.S
 
 std_effects.regen = status_effects.register_effect("regen", {
 	description = S("regen"),
 	fold = function(self, t)
-		return std_effects.util.sum_values(t)
+		return std_effects.util.sum_values(t, s.base_regen_value)
 	end,
 	step_every = 1,
-	step_catchup = true,
+	step_catchup = false, -- we use dtime to scale the effect
 	on_step = function(self, player, value, dtime)
 		if value == 0 then
 			return
 		end
 		local hp = player:get_hp()
+		value = value * dtime * s.regen_rate
 		local int_value = math.floor(value)
 		local float_part = value - int_value
 		if math.random() <= float_part then
